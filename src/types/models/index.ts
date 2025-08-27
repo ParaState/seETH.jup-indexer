e0309eb1cb3d1985576a3b8f20738b04fbb63b8f
huliang <huliang0222@gmail.com>
2025-08-27 18:36:57 +0800

    chore: model

--- src/types/models/index.ts
+++ src/types/models/index.ts
@@ -2,9 +2,8 @@
 
 // Auto-generated , DO NOT EDIT
 
-export {Deposit} from "./Deposit"
+export { Deposit } from "./Deposit";
 
-export {Withdraw} from "./Withdraw"
+export { Withdraw } from "./Withdraw";
 
-export {RedeemRequest} from "./RedeemRequest"
-
+export { RedeemRequest } from "./RedeemRequest";
